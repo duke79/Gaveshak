5d009d0f346284eb34f499b64a4dfd339d27161c
Pulkit Singh <pulkitsingh01@gmail.com>
2017-04-15 14:27:40 +0530

    Multi-thread working

--- src/MultiThreadingFW/OpenCLUse.m/main.cpp
+++ src/MultiThreadingFW/OpenCLUse.m/main.cpp
@@ -1,15 +1,34 @@
 // A simple program that computes the square root of a number
 #include <iostream>
 using namespace std;
-
+#include <cstdlib>
 #define HAVE_STRUCT_TIMESPEC
 #include <pthread.h>
 
-int main(int argc, char *argv[])
-{
-	int a;
-	cin >> a;
-	cout << "Hello World: "<< a;
-	cin >> a;
+#define NUM_THREADS     5
+
+void *PrintHello(void *threadid) {
+	long tid;
+	tid = (long)threadid;
+	cout << "Hello World! Thread ID, " << tid << endl;
+	pthread_exit(NULL);
 	return 0;
+}
+
+int main() {
+	pthread_t threads[NUM_THREADS];
+	int rc;
+	int i;
+
+	for (i = 0; i < NUM_THREADS; i++) {
+		cout << "main() : creating thread, " << i << endl;
+		rc = pthread_create(&threads[i], NULL, PrintHello, (void *)i);
+
+		if (rc) {
+			cout << "Error:unable to create thread," << rc << endl;
+			exit(-1);
+		}
+	}
+
+	pthread_exit(NULL);
 }
