9d8d50bc3cc14c77eb23411bc2bd13a49bb6f3bc
Pulkit Singh <pulkitsingh01@gmail.com>
2017-04-22 15:28:59 +0530

    boost working!

--- src/Gaveshak/Gaveshak_HandleArguments.cpp
+++ src/Gaveshak/Gaveshak_HandleArguments.cpp
@@ -6,7 +6,17 @@
 
 #include <Gaveshak.h>
 
+#include "boost/lambda/lambda.hpp"
+#include "iostream"
+
 void Gaveshak::HandleArguments()
 {
+	using namespace boost::lambda;
+	typedef std::istream_iterator<int> in;
 
+	std::for_each(
+		in(std::cin), in(), std::cout << (_1 * 3) << " ");
+
+	char c;
+	std::cin >> c;
 }
