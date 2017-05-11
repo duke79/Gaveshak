d125035994d1ae9c42aa90f915b80ae38ca81872
Pulkit Singh <pulkitsingh01@gmail.com>
2017-05-11 22:14:53 +0530

    Logger is not required in Globals.h (Globals.h itself may not be required?)

--- src/Utils/include/Globals.h
+++ src/Utils/include/Globals.h
@@ -8,14 +8,6 @@
 
 static std::string     _GaveshakPath;
 
-#define LOG_T CUSTOM_LOG(trace)
-#define LOG_D CUSTOM_LOG(debug)
-#define LOG_I CUSTOM_LOG(info)
-#define LOG_W CUSTOM_LOG(warning)
-#define LOG_E CUSTOM_LOG(error)
-#define LOG_F CUSTOM_LOG(fatal)
-static class Logger *        _Logger;
-
 #define BOOST_ALL_DYN_LINK
 
 #endif
